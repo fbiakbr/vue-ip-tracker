e4311ae70c835d91e9e9036b9bbc7b26ed1d0b86
fbiakbr <febiakbar19@gmail.com>
2022-01-23 16:31:51 +0700

    IP Tracker

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex flex-col h-screen max-h-screen">
+    <!-- Search Result -->
+    <div
+      class="flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32"
+    >
+      <!-- Search Input -->
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
+        <div class="flex">
+          <input
+            class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none"
+            type="text"
+            placeholder="Search for any IP address or leave empty to get your IP info"
+          />
+          <i
+            class="cursor-pointer bg-black text-white px-4 rounded-tr-md rounded-br-md flex items-center fas fa-chevron-right"
+          ></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
